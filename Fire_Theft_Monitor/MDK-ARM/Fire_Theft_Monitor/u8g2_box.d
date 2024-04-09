78101fd8bb0045e36d7146b8678edbe81c142d67
AufrozenH <frozenwzh030314@gmail.com>
2024-04-09 21:54:44 +0800

    commit

--- Fire_Theft_Monitor/MDK-ARM/Fire_Theft_Monitor/u8g2_box.d
+++ Fire_Theft_Monitor/MDK-ARM/Fire_Theft_Monitor/u8g2_box.d
@@ -1,6 +1,6 @@
 fire_theft_monitor/u8g2_box.o: ..\Drivers\Hardware\u8g2\u8g2_box.c \
   ..\Drivers\Hardware\u8g2\u8g2.h ..\Drivers\Hardware\u8g2\u8x8.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdarg.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\limits.h
+  D:\ProgramFiles\Keil5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  D:\ProgramFiles\Keil5\ARM\ARMCLANG\Bin\..\include\stdarg.h \
+  D:\ProgramFiles\Keil5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  D:\ProgramFiles\Keil5\ARM\ARMCLANG\Bin\..\include\limits.h
